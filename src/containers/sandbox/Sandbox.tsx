3910cc37ee7a1e557ec655d771e50e77f6c090da
flatline <deadbrainman@gmail.com>
2022-07-02 23:22:36 +0300

    upload logic refactoring

--- src/containers/sandbox/Sandbox.tsx
+++ src/containers/sandbox/Sandbox.tsx
@@ -3,17 +3,27 @@ import { useEffect } from "react";
 import './Sandbox.scss';
 import Grid from './Grid';
 import { GridCfgDef } from './GridCfg';
+import { EVENTS, on, off} from './../../utils/bus';
 
 const CANVAS_QUERY = 'canvas';
 
 export default function Sandbox() {
   let grid: Grid;
 
+  function onUpload(json: any) {
+    console.log(json);
+  }
+
+  function onDestroy() {
+    off(EVENTS.UPLOAD, onUpload);
+    grid.destroy();
+  }
+
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     grid = new Grid({...GridCfgDef, query: '#' + CANVAS_QUERY});
-
-    return () => grid.destroy()
+    on(EVENTS.UPLOAD, onUpload);
+    return onDestroy;
   });
 
   return <div id={CANVAS_QUERY} className="grid"/>
