129fa02c1129f809058ed73fd71a360687ae1716
flatline <deadbrainman@gmail.com>
2024-09-06 17:41:35 +0300

    refactoring

--- src/components/mode.tsx
+++ src/components/mode.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import FormControl from '@mui/material/FormControl';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Radio from '@mui/material/Radio'
 import { id } from '../utils/utils';
-import { AtomTypes, Modes } from '../enums/enums';
+import { Modes } from '../enums/enums';
 import { store } from '../store/store';
-import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
 
