033af37632afcd668edcb2c685fa89e8dd9d0919
flatline <deadbrainman@gmail.com>
2024-10-05 00:54:22 +0300

    - styles refactoring - starting work with inrma5 VM engine

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import World from 'irma5/src/world'
 import ReactDOM from 'react-dom/client'
 import App from './view/app'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-
+import './index.scss'
+//
+// create react Atomic Playground app
+//
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App/>)
+//
+// create irma5 world instance to run playground atoms
+//
+const w = World()
